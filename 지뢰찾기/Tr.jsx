c141ab27207f53128b331edea71d2c7cf758d3c1
Kyle <tigerlove95@naver.com>
2021-08-17 18:37:39 +0900

    210817

--- 지뢰찾기/Tr.jsx
+++ 지뢰찾기/Tr.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { TableContext } from "./MineSearch";
+import Td from "./Td";
+const Tr = ({ rowIndex }) => {
+  const { tableData } = useContext(TableContext);
+  return (
+    <tr>
+      {tableData[0] &&
+        Array(tableData[0].length)
+          .fill()
+          .map((td, i) => <Td rowIndex={rowIndex} cellIndex={i} />)}
+    </tr>
+  );
+};
 
-const Tr = () => {
-
-}
-
-export default Tr
+export default Tr;
